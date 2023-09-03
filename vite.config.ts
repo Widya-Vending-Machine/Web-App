3f05d0d64c50de7cd084748044d297e51d45f5d2
AndriPGBYK <andrianto.ykhc@gmail.com>
2023-09-04 02:57:22 +0700

    Setup

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import pages from 'vite-plugin-pages';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/Web-App',
+
+  plugins: [
+    // Speed up your Vite dev server with SWC
+    // https://github.com/vitejs/vite-plugin-react-swc
+    react(),
+    // File system based routing for Vue 3 / React / Solid applications using Vite
+    // https://github.com/hannoeru/vite-plugin-pages
+    pages({
+      dirs: 'src/pages',
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+});
